86405f29dc9f55de0a542f8aa16a7e90bb7b5199
Venla <venla.viljamaa@helsinki.fi>
2018-02-12 23:04:42 +0200

    frontti herokuun

--- Viikko_2/viikko_2.6-10/src/services/persons.js
+++ Viikko_2/viikko_2.6-10/src/services/persons.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 const create = (newObject) => {
